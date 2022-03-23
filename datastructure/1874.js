a40f83ddd24390f961dca89a402d5e8805a61062
tjalsrb97 <gnam011@naver.com>
2022-03-23 16:39:07 +0900

    20220323(stack array practice 1874)

--- datastructure/1874.js
+++ datastructure/1874.js
@@ -3,6 +3,30 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : __dirname + '/inp
 let input = fs.readFileSync(filePath).toString().trim().split('\n')
 
 let N = Number(input.shift())
+let stack = []
+let number = []
+let answer = ''
 for(let i=0;i<N;i++){
-    
+    number.push(i+1)
 }
+
+for(let i=0;i<N;i++){
+    let count = 1;
+    console.log(input[i])
+    while(count <= N && stack[stack.length-1] !== Number(input[i])){
+        // console.log(count)
+        stack.push(number.shift())
+        console.log(stack, number)
+        answer += '+\n';
+        count++;
+    }
+    if(stack[stack.length-1] === Number(input[i])){
+        stack.pop();
+        answer += '-\n'
+    }
+    else{
+        answer = 'NO';
+        break;
+    }
+}
+console.log(answer)
